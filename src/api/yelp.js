35673a9a01c62677c4a28483cffbfeb16c5844e8
Divyansh Patel <nealbay@gmail.com>
2022-07-09 01:56:20 -0700

    Delete src/api directory

--- src/api/yelp.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 'Bearer WVdN59eqWKdsolzU7xrc2ZkOWzDjyxXDwjuYLGPCzzhx3hFOGzSPKKYrUH_Hdlr75RWUpfIU_Mw2nCl3iwzlkIy3anda0NUH4Ou7N8sDMqksfq4ER6o5DPxRUqbHYnYx'
-    }
-});
